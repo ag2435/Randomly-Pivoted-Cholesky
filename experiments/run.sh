1300d023f50382aca077112515dfae4a6d84e93a
Albert Gong <ag2435@cornell.edu>
2024-02-11 23:02:48 -0500

    added kt experiment results

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 conda init bash
 conda activate rqcholesky
-papermill qm9.ipynb qm9-20k.ipynb
+papermill qm9.ipynb qm9-100k.ipynb
--- experiments/submit_jobs.sh
+++ experiments/submit_jobs.sh
@@ -3,7 +3,7 @@
 #SBATCH -o out/test_file_%j.out                  # output file (%j expands to jobID)
 #SBATCH -e err/test_file_%j.err                  # error log file (%j expands to jobID)
 #SBATCH -N 1                                 # Total number of nodes requested
-#SBATCH -n 4                                 # Total number of cores requested
+#SBATCH -n 8                                 # Total number of cores requested
 #SBATCH --get-user-env                       # retrieve the users login environment
 #SBATCH --mem=80000                           # server memory (MBs) requested (per node)
 #SBATCH -t 8:00:00                           # Time limit (hh:mm:ss)
